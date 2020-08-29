4f87e56b98700cd7e2d3ac989319be7d6beb4aa1
bizimungu pascal <bizip04@gmail.com>
2020-08-29 22:11:18 +0200

    Connected contact page to fire base
    Grab data from the form to firebase

--- js/main.js
+++ js/main.js
@@ -29,6 +29,27 @@ var initMap = () => {
 }
 
 
+
+// My web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDBTiSjAs-wgFG5S0_-6-DVigzG1xl2u2I",
+    authDomain: "capstone-d4be5.firebaseapp.com",
+    databaseURL: "https://capstone-d4be5.firebaseio.com",
+    projectId: "capstone-d4be5",
+    storageBucket: "capstone-d4be5.appspot.com",
+    messagingSenderId: "22977705906",
+    appId: "1:22977705906:web:dd555d518b695d944b96ab",
+    measurementId: "G-157R635TZE"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+//riference message collection
+var messagesRef = firebase.database().ref('messages');
+
+
 //Validating All the form
 let validateForm = () => {
     var name = document.forms["myForm"]["name1"].value;
@@ -59,6 +80,26 @@ let validateForm = () => {
         errorMessage.classList.add("error__message");
         return false;
     }
+    if ((name !== "" || name !== null) && (email !== "" || email !== null) && (subject !== "" || subject !== null) &&
+        (message !== "" || message !== null)) {
+        document.addEventListener("submit", submitMyForm);
+
+        function submitMyForm(e) {
+            e.preventDefault();
+            //save Message
+            saveMessage(name, email, subject, message);
+            //show alert message in 3 second
+            document.querySelector(".alert__message").style.display = "block";
+            setTimeout(() => {
+                document.querySelector(".alert__message").style.display = "none";
+            }, 3000);
+            //Reset a form after send a message
+            document.getElementById("form__contact").reset();
+
+
+        }
+    }
+
 
 }
 
@@ -111,4 +152,25 @@ let validateSignUpForm = () => {
         errorName.classList.add("error__message");
         return false;
     }
+
+}
+
+// // listrn for subit event
+// document.getElementById("form__contact").addEventListener("submit", submitForm(e));
+
+// function submitForm(e) {
+//     e.preventDefault();
+//     console.log(1234);
+// }
+
+//save the message to firebase
+function saveMessage(name, email, subject, message) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
+    });
+
 }
